b70e7870fddb8c1de7cae4185e3c5b8895c88b7f
torvald2 <valis.source@gmail.com>
2024-10-21 18:12:19 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,11 @@
           Stat
       b-row
         b-col
+          Wallets.mt-2
+      b-row
+        b-col
           PoolTable.mt-2
+
 
 </template>
 
@@ -15,12 +19,15 @@
 import Navbar from '@/components/navbar.vue'
 import Stat from '@/components/stat.vue'
 import PoolTable from '@/components/pool-table.vue'
+import Wallets from '@/components/wallets.vue'
+
 export default {
   name: 'App',
   components: {
     Navbar,
     Stat,
-    PoolTable
+    PoolTable,
+    Wallets
   }
 }
 </script>
---
+++ src/components/wallets.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+    b-card-group
+      b-card( header="Статистика кошельков" bg-variant="success")
+        b-card-text
+          b-list-group
+            b-list-group-item(class="d-flex justify-content-between align-items-center" variant="success")
+             | Всего
+             b-badge(variant="primary" pill) 20.003 NKN 
+            b-list-group-item(class="d-flex justify-content-between align-items-center" variant="success")
+             | Primary (NKNPthYDzwJuPuxNGhdnNjDZN7X6uLt1qRUS)
+             b-badge(variant="warning" pill) 10.001 NKN
+            b-list-group-item(class="d-flex justify-content-between align-items-center" variant="success")
+             | Secondary (NKNPthYDzwJuPuxNGhdnNjDZN7X6uLt1qRUS)
+             b-badge(variant="success" pill) 10.002 NKN
+            
+     
+    
+    </template>
+          
+    <script>
+    
+    
+    export default {
+    name: 'navBar',
+    components: {
+       
+    },
+    methods:{
+       
+    }
+    }
+    </script>
+          
+    <style>
+          
+    </style>
+          
